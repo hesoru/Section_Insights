642f21653c0911700ba68dbd101dc7666ae50a1a
smeans <u8t6d@ugrad.cs.ubc.ca>
2024-10-10 18:14:04 -0700

    fixed test names

--- src/utils/JsonHelper.ts
+++ src/utils/JsonHelper.ts
@@ -204,7 +204,7 @@ export async function getDatasetInfo(id: string, fileName: string): Promise<Insi
 
 /**
  * @returns - Promise<[Map,string,number>,number]>, initializes a map with the names and ids of each file already existing in the ./data folder
- * Will throw an InsightError unable to read a file.
+ * Will throw an InsightError unable to read a file
  */
 export async function getExistingDatasets(): Promise<[Map<string, number>, number]> {
 	const dataPath = "./data";
