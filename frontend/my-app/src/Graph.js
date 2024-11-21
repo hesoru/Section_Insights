cd027515bcc0173f4db0452786694932815e1002
Helena <hesoru@students.cs.ubc.ca>
2024-11-21 04:32:24 -0800

    Completed coding bar charts and line graphs, ran prettier/lint/build.

--- frontend/my-app/src/Graph.js
+++ frontend/my-app/src/Graph.js
@@ -1,9 +1,14 @@
 import {Bar, Line, Pie} from "react-chartjs-2"
-import {Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Tooltip, Title, Legend, ArcElement} from "chart.js"
-import {getPieChartData} from "./InsightsHelper";
+import {Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Tooltip, Title, Legend, ArcElement, BarElement} from "chart.js"
+import {
+	getBottomFiveBarChartData,
+	getLineGraphData,
+	getPieChartData,
+	getTopFiveBarChartData
+} from "./InsightsHelper";
 
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Tooltip, Title, Legend, ArcElement)
-// need labels and datasets.
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Tooltip, Title, Legend, ArcElement, BarElement)
+// need labels and datasets
 // hoverOffset
 
 export const PieChart = ({data}) => {
@@ -13,6 +18,79 @@ export const PieChart = ({data}) => {
     const options = {}
 
     return <Pie options={options} data={data}/>;
+}
+
+export const BarChart = ({data}) => {
+	if(!data || data.length === 0) {
+		return <div>No data found</div>;
+	}
+
+	const allData = data.datasets[0].data;
+	let min = Math.min(...allData) * 0.9;
+	if (min < 0) {
+		min = 0;
+	}
+	let max = Math.max(...allData) * 1.1;
+	if (max > 100) {
+		max = 100;
+	}
+
+	const options = {
+		responsive: true,
+		plugins: {
+			legend: {
+				display: false
+			}
+		},
+		scales: {
+			x: {
+				title: {
+					display: true,
+					text: 'Department',
+				},
+			},
+			y: {
+				title: {
+					display: true,
+					text: 'Average Grade',
+				},
+				min: min,
+				max: max
+			},
+		},
+	};
+
+	return <Bar options={options} data={data}/>;
+}
+
+export const LineGraph = ({data}) => {
+	if(!data || data.length === 0) {
+		return <div>No data found</div>;
+	}
+	const options = {
+		responsive: true,
+		plugins: {
+			legend: {
+				display: false
+			}
+		},
+		scales: {
+			x: {
+				title: {
+					display: true,
+					text: 'Year',
+				},
+			},
+			y: {
+				title: {
+					display: true,
+					text: 'Average Grade',
+				},
+			},
+		},
+	};
+
+	return <Line options={options} data={data}/>;
 }
 
 export async function generatePieCharts({id, department}) {
@@ -25,10 +103,59 @@ export async function generatePieCharts({id, department}) {
     const allPieData = [];
 
     for (const data of pieData) {
-        console.log("looking at data in pieData" + JSON.stringify(data))
+        // console.log("looking at data in pieData" + JSON.stringify(data))
         const department = data.departmentName
         departments.push(department);
         allPieData.push({[department]: data.data});
     }
     return {data: allPieData, departments: departments};
+}
+
+export async function generateBottomFiveBarChart({id, department}) {
+	const barData = (await getBottomFiveBarChartData(id));
+	console.log("barData looking for lowest departments" + JSON.stringify(barData))
+	if (!barData || barData.length === 0) {
+		return {data: {}}
+	}
+	return { data: barData.data };
+}
+
+export async function generateTopFiveBarChart({id, department}) {
+	const barData = (await getTopFiveBarChartData(id));
+	console.log("barData looking for highest departments" + JSON.stringify(barData))
+	if (!barData || barData.length === 0) {
+		return {data: {}}
+	}
+	return { data: barData.data };
+}
+
+export async function generateLineGraphs({id, department}) {
+	const lineData = await getLineGraphData(id);
+	console.log("lineData looking for departments" + JSON.stringify(lineData))
+	if (!lineData || lineData.length === 0) {
+		return {data: {}, departments: []}
+	}
+	const departments = [];
+	const allLineData = {};
+
+	for (const data of lineData) {
+		// console.log("looking at data in lineData" + JSON.stringify(data))
+		const department = data.departmentName
+		if (!departments.includes(department)) {
+			departments.push(department);
+		}
+		if (!allLineData[department]) {
+			allLineData[department] = data.data;
+		}
+		// remove duplicate year data
+		for (let i = 0; i < data.data.labels.length; i++) {
+			const year = data.data.labels[i];
+			const grade = data.data.datasets[0].data[i];
+			if (!allLineData[department].labels.includes(year)) {
+				allLineData[department].labels.push(year);
+				allLineData[department].datasets[0].data.push(grade);
+			}
+		}
+	}
+	return { data: allLineData, departments };
 }
--- frontend/my-app/src/InsightsHelper.js
+++ frontend/my-app/src/InsightsHelper.js
@@ -45,40 +45,62 @@ export async function getPieChartData(id) {
     const results = res.result
     console.log("testing results" + Array.isArray(results))
     const testing = results.map(result => {
-        const firstKey = Object.keys(result)[0]
+        // const firstKey = Object.keys(result)[0]
         return {
             data: {
-                labels: ["Pass", "Fail", "Audit"],
+                labels: ["Pass", "Fail", "Auditing"],
                 datasets: [{
                     data: [result.totalPass, result.totalFail, result.totalAudit],
-                    backgroundColor: ["blue", "green", "red"],
+                    backgroundColor: ['rgb(68,172,103)', 'rgb(255,134,134)', 'rgb(75,192,192)'],
                     hoverOffset: 4
                 }],
             },
-            departmentName: result[firstKey]
+            departmentName: result[`${id}_dept`]
         };
     });
     console.log("after map testing array" + JSON.stringify(testing));
     return testing;
 }
 
-export async function getBarGraphData(id) {
+export async function getBottomFiveBarChartData(id) {
+	console.log("this is the id" + id);
 	const query_asc = {
 		"WHERE": {},
 		"OPTIONS": {
-		"COLUMNS": [`${id}_dept`, "avgGrade"],
+			"COLUMNS": [`${id}_dept`, "avgGrade"],
 			"ORDER": {
-			"dir": "UP",
+				"dir": "UP",
 				"keys": ["avgGrade"]
-		}
-	},
+			}
+		},
 		"TRANSFORMATIONS": {
-		"GROUP": [`${id}_dept`],
+			"GROUP": [`${id}_dept`],
 			"APPLY": [
-			{ "avgGrade": { "AVG": `${id}_avg` } }
+				{ "avgGrade": { "AVG": `${id}_avg` } }
 			]
 		}
 	}
+	console.log("this is the query_asc" + JSON.stringify(query_asc, null, 2));
+	const res_asc = await performQueryAPI(query_asc);
+	const results_asc = res_asc.result;
+	const results_bottom_five = results_asc.slice(0, 5);
+	console.log("looking at bottom 5" + JSON.stringify(results_bottom_five));
+	return {
+		data: {
+			labels: results_bottom_five.map(result => result[`${id}_dept`]),
+			datasets: [{
+				label: "Average Grade",
+				data: results_bottom_five.map(result => result.avgGrade),
+				backgroundColor: 'rgb(255,134,134)',
+				borderColor: 'rgb(255,134,134)',
+				borderWidth: 1
+			}]
+		}
+	};
+}
+
+export async function getTopFiveBarChartData(id) {
+	console.log("this is the id" + id);
 	const query_desc = {
 		"WHERE": {},
 		"OPTIONS": {
@@ -95,33 +117,63 @@ export async function getBarGraphData(id) {
 			]
 		}
 	}
-	const res_asc = await performQueryAPI(query_asc);
+	console.log("this is the query_desc" + JSON.stringify(query_desc, null, 2));
 	const res_desc = await performQueryAPI(query_desc);
-	const results_asc = res_asc.body.result
-	const results_desc = res_desc.body.result
-	const bottomFive = results_asc.map(result => {
-		return {
-			labels: ["Pass", "Fail", "Audit"],
+	const results_desc = res_desc.result;
+	const results_top_five = results_desc.slice(0, 5);
+	console.log("looking at top 5" + JSON.stringify(results_top_five));
+	return {
+		data: {
+			labels: results_top_five.map(result => result[`${id}_dept`]),
 			datasets: [{
-				data: [result.totalPass, result.totalFail, result.totalAudit],
-				backgroundColor: ["blue", "green", "red"],
-				hoverOffset: 4
-			}],
-			departmentName: result[0]
-		};
-	});
-	const topFive = results_desc.map(result => {
-		return {
-			labels: ["Pass", "Fail", "Audit"],
-			datasets: [{
-				data: [result.totalPass, result.totalFail, result.totalAudit],
-				backgroundColor: ["blue", "green", "red"],
-				hoverOffset: 4
-			}],
-			departmentName: result[0]
-		};
-	});
+				label: "Average Grade",
+				data: results_top_five.map(result => result.avgGrade),
+				backgroundColor: 'rgb(68,172,103)',
+				borderColor: 'rgb(68,172,103)',
+				borderWidth: 1
+			}]
+		}
+	};
 }
 
-
-
+export async function getLineGraphData(id) {
+	console.log("this is the id" + id);
+	const query = {
+		"WHERE": {},
+		"OPTIONS": {
+			"COLUMNS": [`${id}_dept`, `${id}_year`, "avgGrade"],
+			"ORDER": {
+				"dir": "UP",
+				"keys": [`${id}_dept`, `${id}_year`]
+			}
+		},
+		"TRANSFORMATIONS": {
+			"GROUP": [`${id}_dept`, `${id}_year`],
+			"APPLY": [
+				{ "avgGrade": { "AVG": `${id}_avg` } }
+			]
+		}
+	}
+	console.log("this is the query" + JSON.stringify(query, null, 2));
+	const res = await performQueryAPI(query);
+	const results = res.result;
+	console.log("looking at avgByYear" + JSON.stringify(results));
+	const avgByYear = results.map(result => {
+		return {
+			data: {
+				labels: [result[`${id}_year`]],
+				datasets: [{
+					label: `Average Grade for ${result[`${id}_dept`]}`,
+					data: [result.avgGrade],
+					backgroundColor: 'rgb(75, 192, 192)',
+					pointBackgroundColor: 'rgb(75, 192, 192)',
+					borderColor: 'rgb(75, 192, 192)',
+					tension: 0.1
+				}]
+			},
+			departmentName: result[`${id}_dept`]
+		};
+	});
+	console.log("after map testing array" + JSON.stringify(avgByYear));
+	return avgByYear;
+}
